365f0c8f69f58f2e255fb7757c3d6360ab0e7e5d
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-22 08:35:04 -0800

    Change html column from TEXT to MEDIUMTEXT

--- sql/leogalle_test/webpage/create.sql
+++ sql/leogalle_test/webpage/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `webpage` (
       `webpage_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
       `website_id` int(10) unsigned NOT NULL,
-      `url` varchar(255) NOT NULL,
-      `title` varchar(255) NOT NULL,
-      `html` text NOT NULL,
+      `url` VARCHAR(255) NOT NULL,
+      `title` VARCHAR(255) NOT NULL,
+      `html` MEDIUMTEXT NOT NULL,
       PRIMARY KEY (`webpage_id`),
       KEY `website_id` (`website_id`),
       CONSTRAINT `website_id` FOREIGN KEY (`website_id`) REFERENCES `website` (`website_id`) ON DELETE CASCADE ON UPDATE CASCADE

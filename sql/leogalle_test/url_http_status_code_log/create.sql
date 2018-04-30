2a4f173196db644739bfa260fc240f01b8ce6be8
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-30 06:53:45 +0000

    Create index on url column

--- sql/leogalle_test/url_http_status_code_log/create.sql
+++ sql/leogalle_test/url_http_status_code_log/create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `url_http_status_code_log` (
-      `url_http_status_code_log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-      `url` varchar(255) NOT NULL,
-      `http_status_code` INT(3) NOT NULL,
-      `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-      PRIMARY KEY (`url_http_status_code_log_id`)
+    `url_http_status_code_log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `url` varchar(255) NOT NULL,
+    `http_status_code` INT(3) NOT NULL,
+    `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`url_http_status_code_log_id`),
+    INDEX (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

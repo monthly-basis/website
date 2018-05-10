e91bd1880428f47b3dee5f4ba03095f504782750
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-10 14:22:01 +0000

    Change website_id column to default null

--- sql/leogalle_test/webpage/create.sql
+++ sql/leogalle_test/webpage/create.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `webpage` (
-      `webpage_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-      `website_id` int(10) unsigned NOT NULL,
-      `url` VARCHAR(255) NOT NULL,
-      `title` VARCHAR(255) NOT NULL,
-      `html` MEDIUMTEXT NOT NULL,
-      PRIMARY KEY (`webpage_id`),
-      KEY `website_id` (`website_id`)
+    `webpage_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `website_id` int(10) unsigned DEFAULT NULL,
+    `url` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `html` MEDIUMTEXT NOT NULL,
+    PRIMARY KEY (`webpage_id`),
+    KEY `website_id` (`website_id`),
+    UNIQUE `url` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

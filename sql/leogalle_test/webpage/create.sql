b65c0337c29f058df056bfe57fc7157b9fd35e45
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-12 21:06:00 +0000

    Update html column so that it can be null

--- sql/leogalle_test/webpage/create.sql
+++ sql/leogalle_test/webpage/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE `webpage` (
     `website_id` int(10) unsigned DEFAULT NULL,
     `url` VARCHAR(255) NOT NULL,
     `title` VARCHAR(255) NOT NULL,
-    `html` MEDIUMTEXT NOT NULL,
+    `html` MEDIUMTEXT DEFAULT NULL,
     PRIMARY KEY (`webpage_id`),
     KEY `website_id` (`website_id`),
     UNIQUE `url` (`url`)

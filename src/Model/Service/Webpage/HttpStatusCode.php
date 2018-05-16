60574e0617312c4f717f069160ab64a0e94c7a11
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-16 03:02:37 +0000

    Add timeout for curl function

--- src/Model/Service/Webpage/HttpStatusCode.php
+++ src/Model/Service/Webpage/HttpStatusCode.php
@@ -14,15 +14,16 @@ class HttpStatusCode
     public function getHttpStatusCode(
         WebsiteEntity\Webpage $webpageEntity
     ) : int {
-		$handle = curl_init($webpageEntity->getUrl());
-		curl_setopt($handle,  CURLOPT_RETURNTRANSFER, true);
+        $handle = curl_init($webpageEntity->getUrl());
+        curl_setopt($handle,  CURLOPT_RETURNTRANSFER, true);
         curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 3);
 
-		$response = curl_exec($handle);
+        $response = curl_exec($handle);
 
-		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-		curl_close($handle);
+        curl_close($handle);
 
         return $httpCode;
 

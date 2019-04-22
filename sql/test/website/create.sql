d623d8bf13d2f75f69a946f1454e47c1c40af556
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-04-22 20:26:09 +0000

    Increase column lengths

--- sql/test/website/create.sql
+++ sql/test/website/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `website` (
       `website_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-      `domain` varchar(32) NOT NULL,
-      `name` varchar(32) NOT NULL,
+      `domain` varchar(255) NOT NULL,
+      `name` varchar(255) NOT NULL,
       `description` varchar(255) NOT NULL,
       `google_analytics_tracking_id` varchar(16) DEFAULT NULL,
-      `amazon_tracking_id` varchar(32) DEFAULT NULL,
+      `amazon_tracking_id` varchar(255) DEFAULT NULL,
       `facebook_app_id` bigint unsigned DEFAULT NULL,
       PRIMARY KEY (`website_id`),
       UNIQUE KEY `domain` (`domain`)

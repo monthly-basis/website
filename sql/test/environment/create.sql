6c68655230e2b73ab550a1cbf035f1e597b3689f
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-04-29 20:00:35 +0000

    Write sql files for environment table

---
+++ sql/test/environment/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `environment` (
+      `environment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+      `name` varchar(255) NOT NULL,
+      PRIMARY KEY (`environment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
